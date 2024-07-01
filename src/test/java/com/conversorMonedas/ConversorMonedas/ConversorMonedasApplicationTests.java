9767f41926255853d778bcb3765f650938f7642b
Lautaro Orellano <Lautaa123@gmail.com>
2024-07-01 13:50:38 -0300

    Carpeta resource y pom.xml actualizado

---
+++ src/test/java/com/conversorMonedas/ConversorMonedas/ConversorMonedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversorMonedas.ConversorMonedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorMonedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
